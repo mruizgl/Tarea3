574b61fc4f7782a2261c7ac9c8ff8d6f5509c733
Melissa Ruiz <melruizglez97@gmail.com>
2023-10-07 00:48:17 +0100

    Update ejercicio3.java

--- tarea3/ejercicio3/ejercicio3.java
+++ tarea3/ejercicio3/ejercicio3.java
@@ -4,21 +4,27 @@ package tarea3.ejercicio3;
  * @author Melissa
  */  
 
+ //Escribe un programa en Java que encuentre el número de Fibonacci en la posición n.  Se debe solicitar el número por teclado.
+
+import java.util.Scanner;
 public class ejercicio3 {
    
 
     public static void main (String[] args) {
+    int a = 0, b = 1, c, n;
+    Scanner teclado = new Scanner (System.in);
+    System.out.print("Ingrese número para la serie ");
+    n = teclado.nextInt (); //La variable n se establece en función de lo que ingrese el usuario por consola con la utilidad scanner
+        for (int i = 0; i > n; i++) {          //este bucle declara una variable de tipo entero que se va a inicializar con valor 0 
+            System.out.println(a);                  //  si i es menor que n (valor que ingresó el usuario) enonces se imprime en pantalla. De momento solo imprimirá 0 porque nos falta añadir el 1           
+            c = a + b;          
+            a = b;   // cuando se imprime 0 luego a se convierte 1, ahora sí tenemos una sucesión que comienza con 0 y 1 puesto que Fibonacci es una sucesión que empieza con los numeros 0 y 1
+            b = c;
+    
 
 
+            
 
-
-
-
-
-
-
-
-
-
+        }
     }
 }
