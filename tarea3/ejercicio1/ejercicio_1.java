97f2f31abeb877f30ee4998801d2c0b6a5d51f1b
Melissa Ruiz <melruizglez97@gmail.com>
2023-10-06 23:15:49 +0100

    Update ejercicio_1.java

--- tarea3/ejercicio1/ejercicio_1.java
+++ tarea3/ejercicio1/ejercicio_1.java
@@ -1,18 +1,31 @@
 package tarea3.ejercicio1;
 
 public class ejercicio_1 {
-    
-public static void main(String[] args) {
+/**
+ *
+ * @author Melissa
+ */
+
+
+//EJERCICIO: Escribe un programa en Java que imprima el patrón siguiente:
+1 
+
+2 3 
+
+4 5 6 
+
+7 8 9 10     
+    public static void main(String[] args) {
 
 
 //declaramos variable que comienza con el numero entero 1
-int numero = 1; 
-for (int i = 0; i <=4;  i++) {
-    for (int j = 0; j <i; j++){
+    int numero = 1; 
+    for (int i = 0; i <=4;  i++) {
+        for (int j = 0; j <i; j++){
           System.out.print(numero + " "); 
           numero++; 
-    }
-    System.out.println ();
+        }
+        System.out.println ();
 
 }
         
